49a70847c0a9afbda999e3f367ef1038c9e105d5
flextechlda <flextechlda@gmail.com>
2024-05-24 04:22:58 -0700

    changed app version

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.get("/", async (request, response) => {
 
 app.get("/api/current-mobile-app-version", async (request, response) => {
     response.json({
-        current_mobile_app_version: "1.0.2",
+        current_mobile_app_version: "1.0.1",
         download_link: "https://expo.dev/artifacts/eas/pBKjgxqGgiKM27XpX7UKBW.apk",
     });
 });
